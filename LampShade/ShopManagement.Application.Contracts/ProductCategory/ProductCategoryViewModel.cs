9333e34b1f415a73f3c64239032eb5700154410f
Hamidghalati <umberela4u@yahoo.com>
2022-06-05 15:23:28 +0430

    Domain And Application.Contracts Layer Added To project

---
+++ LampShade/ShopManagement.Application.Contracts/ProductCategory/ProductCategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ShopManagement.Application.Contracts.ProductCategory
+{
+    
+        public class ProductCategoryViewModel
+        {
+            public long Id { get; set; }
+            public string Name { get; set; }
+            public string Picture { get; set; }
+            public string DateCreate { get; set; }
+            public long ProductsCount { get; set; }
+
+        }
+    
+}
---
+++ LampShade/ShopManagement.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ShopManagement.Application
+{
+    public class Class1
+    {
+    }
+}
