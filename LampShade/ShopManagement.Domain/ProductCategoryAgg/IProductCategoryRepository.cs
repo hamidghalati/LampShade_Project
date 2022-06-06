f258b0c9bba91bfbd19cc9013227074c34cc78ea
Hamidghalati <umberela4u@yahoo.com>
2022-06-06 21:11:49 +0430

    Infrastructure and Framework Changes

--- LampShade/ShopManagement.Domain/ProductCategoryAgg/IProductCategoryRepository.cs
+++ LampShade/ShopManagement.Domain/ProductCategoryAgg/IProductCategoryRepository.cs
@@ -2,16 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using _0_Framework.Domain;
 
 namespace ShopManagement.Domain.ProductCategoryAgg
 {
-    public interface IProductCategoryRepository
+    public interface IProductCategoryRepository : IRepository<long, ProductCategory>
     {
-        void Create(ProductCategory entity);
-        ProductCategory Get(long id);
-        List<ProductCategory> GetAll();
-        bool Exists(Expression<Func<ProductCategory,bool>> expression);
-        void SaveChanges();
         EditProductCategory GetDetails(long id);
         List<ProductCategoryViewModel> Search(ProductCategorySearchModel searchModel);
     }
---
+++ LampShade/ShopManagement.Infrastructure .EFCore/ShopContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ShopManagement.Domain.ProductCategoryAgg;
+using ShopManagement.Infrastructure_.EFCore.Mapping;
+
+namespace ShopManagement.Infrastructure_.EFCore
+{
+    public class ShopContext:DbContext
+    {
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options)
+        {
+        }
+
+        protected ShopContext(DbSet<ProductCategory> productCategories)
+        {
+            ProductCategories = productCategories;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var assembly = typeof(ProductCategoryMapping).Assembly;
+            modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
